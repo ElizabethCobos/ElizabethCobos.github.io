01cdb26d9fe5fa3545851c650032390249be7c28
ElizabethCobos <110860938+ElizabethCobos@users.noreply.github.com>
2022-09-05 18:07:33 -0500

    Add files via upload

---
+++ cv-personal.py
@@ -0,0 +1,92 @@
+<html>
+<body>
+
+<table border="1">
+
+<tr>
+
+<td>
+
+<h1>Manuel Carrion Castanon</h1>
+
+<h2>Lic.Inteligencia de negocios</h2>
+
+<p>
+Mi objetivo en la vida se centra en contribuir en una empresa de renombre como Google, donde trabajando mutuamente logremos desarrollar tanto mis habilidades profesionales como desarrollo organizacional.
+</p>
+
+</td>
+<td>
+<img src="yo.jpeg" alt="yo" width="200" height="200">
+</td>
+
+<td>
+<ul>
+<li>A01734873@tec.mx </li>
+<li>Telefono 2292653976 </li>
+<li>San Andres, Ver. Mexico </li>
+<li>Instagram manucarrionc </li>
+</ul>
+</td>
+
+</tr>
+
+<tr>
+
+<td>
+
+<h1>Experiencia Laboral</h1>
+
+<ol>
+<li>
+<ul>
+<li> CEO </li>
+<li> Google </li>
+<li> 2030 - 2037 </li>
+<li> Dublin, Irlanda </li>
+
+<li>
+<ol>
+<li>Aumentar las capacitaciones un 40%</li>
+<li>Realizar 10 programas de ayuda social</li>
+
+</ol>
+
+</li>
+
+</ul>
+</li>
+
+<ul>
+<li> Director de ventas </li>
+<li> Amazon </li>
+<li> 2037 - 2040 </li>
+<li> London, Inglaterra </li>
+</ul>
+
+</td>
+
+<td>
+<h1>Habilidades</h1>
+<ul>
+<li> Liderazgo </li>
+<li> Comunicacion </li>
+<li> Motivacion </li>
+<li> Proactivo </li>
+<li> Capacidad de resolución de problemas </li>
+<li> Trabajo en equipo </li>
+</ul>
+</td>
+
+</tr>
+
+<tr>
+<td>
+<h1>Educacion</h1>
+</td>
+</tr>
+
+</table>
+
+</body>
+</html>
